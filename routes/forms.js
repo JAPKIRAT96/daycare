dbf6e9531900052e8540ab4a63aa64f9a244e7d6
Japkirat Singh <japkiratsingh@MacBook-Pro.local>
2021-01-22 21:15:12 -0800

    pushing again with complete code

---
+++ routes/forms.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+  app.get("/newParent", function(req, res) {
+    res.render("newparent");
+  });
+
+  app.get("/newChild/:pid", function(req, res) {
+    res.render("newchild", { parentId: req.params.pid });
+  });
+
+  app.get("/scheduleChild", function(req, res) {
+    res.render("schedulechild");
+  });
+};
